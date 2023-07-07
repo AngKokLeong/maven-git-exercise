b6a8fbe111fcdb924823756f99f52afa86a38141
Ang Kok Leong <angkokleong@Apex.local>
2023-07-07 16:15:03 +0800

    modified the group id to com.sddevops.maven.eclipse

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -1,4 +1,4 @@
-package com.example.mavengitexercise;
+package com.sddevops.maven.eclipse;
 
 /**
  * Hello world!
--- src/test/java/com/sddevops/maven/eclipse/AppTest.java
+++ src/test/java/com/sddevops/maven/eclipse/AppTest.java
@@ -1,4 +1,4 @@
-package com.example.mavengitexercise;
+package com.sddevops.maven.eclipse;
 
 import static org.junit.Assert.assertTrue;
 
